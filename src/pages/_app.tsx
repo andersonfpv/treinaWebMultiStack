789da73b5b06cca394ba029950c65afea9236ee4
Anderson Felipe <anderson_fpv@yahoo.com.br>
2021-06-11 23:36:59 -0300

    Concluindo o template e conectando com a api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ThemeProvider } from '@material-ui/core';
 import theme from 'ui/themes/theme';
 import Head from 'next/head';
 import Header from 'ui/componentes/surfaces/Header/Header';
+import Footer from 'ui/componentes/surfaces/Footer/Footer';
+import { AppContainer } from 'ui/styles/pages/_app.style';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -14,8 +16,11 @@ function MyApp({ Component, pageProps }) {
         <link rel="stylesheet" href="/fonts/tw-icons/css/treinaweb-icons.css" />
       </Head>
       <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
+        <AppContainer>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
